8fed60c931a2ccdb6b2df33d481013a64efeb502
Shomail <shomail1988@gmail.com>
2019-01-11 22:35:16 +0100

    prototypes in constructor functions and new keyword

--- 32 - Prototypal inheritance/app.js
+++ 32 - Prototypal inheritance/app.js
@@ -20,3 +20,29 @@ Object.setPrototypeOf(nameObj, protoObj);
 console.log(nameObj);
 //now we have achived inheritance, as lastName will be read from up the chain
 console.log(nameObj.lastName);
+
+//prototype delegation with new keyword
+//here the newly created object myCar prototype refers to the prototype of constructor function
+//prototype objects are created when functions are declared
+//by default they have only one accessible property called constructor
+//we can add new propperties to this prototype obj and it is available to the instance with new keyword
+//constructor prop on prototype obj holds the value of function
+function Car(make) {
+  this.make = make;
+}
+
+console.log(Car.prototype);
+console.log(Car.prototype.constructor);
+
+Car.prototype.wheels = 4;
+
+console.log(Car.prototype);
+
+const myCar = new Car('BMW');
+
+console.log(myCar);
+
+//if we set the prototype obj to empty obj
+//then constructor prop refers to global object type
+//Car.prototype = {}
+//console.log(Car.prototype.constructor) // Object
